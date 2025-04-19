b9b43822534dc01d5ac51e5c60af646be82c2b35
srujanakokkera <srujana.kokkera0716@gmail.com>
2025-04-19 17:14:27 +0530

    eleventh commit

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+mysqlconnector://root:sruju123@db4free.net:3306/employeedb"
+DATABASE_URL = "mysql+mysqlconnector://root:sruju123@127.0.0.1:3306/employeedb"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
